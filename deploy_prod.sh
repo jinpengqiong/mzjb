dbddb635a4174cc68fe516bc7903f5882d2e8cd8
King <jinpengguan@126.com>
2018-09-30 14:34:58 +0800

    optimized the layout of the UI

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -2,4 +2,8 @@
 
 rm -r '../../muzhijubao_web_release/prod/'
 cp -r './out/.' '../../muzhijubao_web_release/prod'
+cd '../../muzhijubao_web_release/'
+git add -A
+git commit -m'deploy prod repo'
+git push origin master
 echo 'done'
